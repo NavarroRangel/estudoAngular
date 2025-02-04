da9702a16fe23e8f877b4dfce2dcca0ab787d826
Navarro rangel de souza <navarrorangel1@hotmail.com>
2025-02-04 10:25:31 -0300

    add: adicionado comunicacao com backend

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamento',
@@ -6,24 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listar-pensamento.component.css']
 })
 export class ListarPensamentoComponent implements OnInit {
-  listaPensamentos = [
-    {
-      conteudo: 'Passo informações entre componentes',
-      autoria: 'Componente pai',
-      modelo: 'modelo1'
-    },
-    {
-      conteudo: 'Minha proproiedade é decorada com @input',
-      autoria: 'navarro',
-      modelo: 'modelo1'
-    },
-    {
-      conteudo: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.',
-      autoria: 'navarro',
-      modelo: 'modelo1'
-    },
-
-  ]
+  listaPensamentos:Pensamento[] = [];
 
   constructor() { }
 
