8d22ef783d43b05bcbcb4faa5aa53b1b19778af7
Navarro rangel de souza <navarrorangel1@hotmail.com>
2025-02-04 10:16:37 -0300

    add: interface e backend

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,16 +7,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentoComponent implements OnInit {
   listaPensamentos = [
-    // {
-    //   conteudo: 'Passo informações entre componentes',
-    //   autoria: 'Componente pai',
-    //   modelo: 'modelo1'
-    // },
-    // {
-    //   conteudo: 'Minha proproiedade é decorada com @input',
-    //   autoria: 'navarro',
-    //   modelo: 'modelo1'
-    // }
+    {
+      conteudo: 'Passo informações entre componentes',
+      autoria: 'Componente pai',
+      modelo: 'modelo1'
+    },
+    {
+      conteudo: 'Minha proproiedade é decorada com @input',
+      autoria: 'navarro',
+      modelo: 'modelo1'
+    },
+    {
+      conteudo: 'lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi. lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec, ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa, varius a, semper congue, euismod non, mi.',
+      autoria: 'navarro',
+      modelo: 'modelo1'
+    },
 
   ]
 
