4b09bdfb0b0b37c947148c1084cae89b6230daed
Navarro rangel de souza <navarrorangel1@hotmail.com>
2025-02-05 09:11:24 -0300

    add: form reativo

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PensamentoService } from '../pensamento.service';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -9,21 +10,24 @@ import { Router } from '@angular/router';
 })
 export class CriarPensamentoComponent implements OnInit {
 
-  pensamento = {
-    conteudo : '',
-    autoria: '',
-    modelo: 'modelo1'
-  }
+
+  formulario!: FormGroup;
 
   constructor(
     private service: PensamentoService,
-    private router : Router
+    private router : Router,
+    private formBuilder : FormBuilder
   ) { }
 
   ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      conteudo:['formulário reativo'],
+      autoria: ['Angular'],
+      modelo: 'moedelo1'
+    })
   }
   criaPensamento(){
-    this.service.criar(this.pensamento).subscribe(()=>{
+    this.service.criar(this.formulario.value).subscribe(()=>{
       this.router.navigate(['./listarPensamento'])
     })
   }
